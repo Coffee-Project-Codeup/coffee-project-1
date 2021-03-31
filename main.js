e147fe69b4f84e3dc019dd5863955943a800ec5b
Darius-Brown <78042299+Darius-Brown@users.noreply.github.com>
2021-03-31 09:48:14 -0500

    Update main.js

--- main.js
+++ main.js
@@ -18,17 +18,6 @@ function renderCoffee(coffee) {
 
     return html;
 }
-*/
-
-function renderCoffee(coffee) {
-    var html = '<div class="coffee">';
-    html += '<p>' + coffee.id + '</p>';
-    html += '<p>' + coffee.name + '</p>';
-    html += '<p>' + coffee.roast + '</p>';
-    html += '</div>';
-
-    return html;
-}
 
 function renderCoffees(coffees) {
     var html = '';
